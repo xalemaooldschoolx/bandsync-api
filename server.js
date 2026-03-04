7a77a37c61a0fe5ef4fa14b8de7b3759280951cb
xalemaooldschoolx <xalemaoxoldschool@gmail.com>
2026-03-04 10:07:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: '🎸 BandSync API 2.0 ONLINE!' });
-// Instagram REAL OAuth
+});
+  
+  // Instagram REAL OAuth
 app.get('/instagram/auth-url', (req, res) => {
   const instagramAuthUrl = `https://www.facebook.com/v20.0/dialog/oauth?client_id=TEU_APP_ID_AQUI&redirect_uri=https://bandsync-api-production-b004.up.railway.app/instagram/callback&scope=instagram_basic,pages_show_list,instagram_manage_insights&response_type=code`;
   res.json({ url: instagramAuthUrl });
@@ -75,4 +77,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 BandSync 2.0: http://localhost:${PORT}`);
 });
+
 
